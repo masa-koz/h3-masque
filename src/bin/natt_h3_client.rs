3ce57175c8e8cd3c7c7588a423d1768a9ca3bd1a
Masahiro Kozuka <masa.koz@kozuka.jp>
2026-01-04 21:58:17 +0900

    nat-t

---
+++ src/bin/natt_h3_client.rs
@@ -0,0 +1,188 @@
+/// This file is based on the `client.rs` example from the `h3` crate.
+use argh::FromArgs;
+use futures::future;
+use h3::error::{ConnectionError, StreamError};
+use h3_msquic_async::msquic;
+use h3_msquic_async::msquic_async;
+use rand::Rng;
+use std::env;
+use std::fs::OpenOptions;
+use std::future::poll_fn;
+use tokio::io::AsyncWriteExt;
+use tracing::{error, info};
+
+#[derive(FromArgs, Clone)]
+/// client args
+struct CmdOptions {
+    /// target server address
+    #[argh(option, default = "String::from(\"127.0.0.1:4443\")")]
+    target: String,
+}
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cmd_opts: CmdOptions = argh::from_env();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
+        .with_writer(std::io::stderr)
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
+    let registration = msquic::Registration::new(&msquic::RegistrationConfig::default())?;
+
+    let alpn = [msquic::BufferRef::from("h3")];
+    let settings = msquic::Settings::new()
+        .set_IdleTimeoutMs(10000)
+        .set_KeepAliveIntervalMs(1000)
+        .set_PeerBidiStreamCount(100)
+        .set_PeerUnidiStreamCount(100)
+        .set_DatagramReceiveEnabled()
+        .set_StreamMultiReceiveEnabled()
+        .set_AddAddressMode(msquic::AddAddressMode::NatTraversal);
+    let configuration = msquic::Configuration::open(&registration, &alpn, Some(&settings))?;
+    let cred_config = msquic::CredentialConfig::new_client()
+        .set_credential_flags(msquic::CredentialFlags::NO_CERTIFICATE_VALIDATION);
+    configuration.load_credential(&cred_config)?;
+
+    let conn = msquic_async::Connection::new(&registration)?;
+    if let Ok(sslkeylogfile) = env::var("SSLKEYLOGFILE") {
+        info!("SSLKEYLOGFILE is set: {}", sslkeylogfile);
+        conn.set_sslkeylog_file(
+            OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(sslkeylogfile)?,
+        )?;
+    }
+    conn.set_share_binding(true)?;
+    let candidate_addr: std::net::SocketAddr = format!(
+        "153.127.33.247:{}",
+        rand::thread_rng().gen_range(1025..65535)
+    )
+    .parse()?;
+    conn.add_candidate_addr(candidate_addr.clone(), candidate_addr)?;
+
+    let target = cmd_opts
+        .target
+        .parse::<std::net::SocketAddr>()
+        .map_err(|e| anyhow::anyhow!("failed to parse target address: {}", e))?;
+    conn.start(&configuration, &target.ip().to_string(), target.port())
+        .await?;
+
+    let local_addr = conn.get_local_addr()?;
+    info!("connected from {} to {}", local_addr, target);
+
+    let event_handle = {
+        let conn = conn.clone();
+        tokio::task::spawn(async move {
+            while let Ok(event) = poll_fn(|cx| conn.poll_event(cx)).await {
+                match event {
+                    msquic_async::ConnectionEvent::NotifyObservedAddress {
+                        local_address,
+                        observed_address,
+                    } => {
+                        info!(
+                            "local address: {}, observed address: {}",
+                            local_address, observed_address
+                        );
+                    }
+                    msquic_async::ConnectionEvent::NotifyRemoteAddressAdded {
+                        address,
+                        sequence_number,
+                    } => {
+                        info!(
+                            "Added remote address: {}, sequence number: {}",
+                            address, sequence_number
+                        );
+                    }
+                    msquic_async::ConnectionEvent::PathValidated {
+                        local_address,
+                        remote_address,
+                    } => {
+                        info!(
+                            "path validated local address: {}, remote address: {}",
+                            local_address, remote_address
+                        );
+                    }
+                    msquic_async::ConnectionEvent::NotifyRemoteAddressRemoved {
+                        sequence_number,
+                    } => {
+                        info!(
+                            "Removed remote address with sequence number: {}",
+                            sequence_number
+                        );
+                    }
+                }
+            }
+            anyhow::Ok(())
+        })
+    };
+    let h3_conn = h3_msquic_async::Connection::new(conn);
+    let (mut driver, mut send_request) = h3::client::new(h3_conn).await?;
+
+    let drive = async move {
+        Err::<(), ConnectionError>(future::poll_fn(|cx| driver.poll_close(cx)).await)
+    };
+    let request = async move {
+        info!("sending request ...");
+
+        let req = http::Request::builder()
+            .uri("https://127.0.0.1:8443/")
+            .header("x-advertise-address", format!("{}", local_addr))
+            .body(())?;
+
+        // sending request results in a bidirectional stream,
+        // which is also used for receiving response
+        let mut stream = send_request.send_request(req).await?;
+
+        // finish on the sending side
+        stream.finish().await?;
+
+        info!("receiving response ...");
+
+        let resp = stream.recv_response().await?;
+
+        info!("response: {:?} {}", resp.version(), resp.status());
+        info!("headers: {:#?}", resp.headers());
+
+        // `recv_data()` must be called after `recv_response()` for
+        // receiving potential response body
+        while let Some(mut chunk) = stream.recv_data().await? {
+            let mut out = tokio::io::stdout();
+            out.write_all_buf(&mut chunk).await?;
+            out.flush().await?;
+        }
+
+        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+
+        Ok::<_, anyhow::Error>(())
+    };
+
+    let (req_res, drive_res) = tokio::join!(request, drive);
+    if let Err(err) = req_res {
+        match err.downcast::<StreamError>() {
+            Ok(err) => {
+                if err.is_h3_no_error() {
+                    info!("connection closed with H3_NO_ERROR");
+                } else {
+                    error!("request failed: {:?}", err);
+                }
+            }
+            Err(err) => {
+                error!("request failed: {:?}", err);
+            }
+        }
+    }
+    if let Err(err) = drive_res {
+        if err.is_h3_no_error() {
+            info!("connection closed with H3_NO_ERROR");
+        } else {
+            error!("connection closed with error: {:?}", err);
+            return Err(err.into());
+        }
+    }
+
+    event_handle.await??;
+    Ok(())
+}
---
+++ src/bin/natt_h3_server.rs
@@ -0,0 +1,317 @@
+use std::{future::poll_fn, net::SocketAddr, path::PathBuf, sync::Arc};
+
+use argh::FromArgs;
+use bytes::{Bytes, BytesMut};
+use h3::{quic::BidiStream, server::RequestStream};
+use h3_msquic_async::{msquic, msquic_async};
+use http::{Request, StatusCode};
+use tokio::sync::mpsc;
+use tokio::{fs::File, io::AsyncReadExt};
+use tracing::{error, info};
+
+#[derive(FromArgs, Clone)]
+/// server args
+pub struct CmdOptions {
+    /// root path
+    #[argh(option)]
+    root: Option<PathBuf>,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
+        .with_writer(std::io::stderr)
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let cmd_opts: CmdOptions = argh::from_env();
+    let root = Arc::new(cmd_opts.root);
+
+    let local_bind_addr: SocketAddr = "127.0.0.1:8443".parse()?;
+    let server_addr: SocketAddr = "153.127.33.247:4443".parse()?;
+    let target_addr: Option<SocketAddr> = None;
+
+    let registration = msquic::Registration::new(&msquic::RegistrationConfig::default())?;
+
+    let (_handle, mut event_receiver) = h3_masque::client::connect_udp_bind_proxy(
+        &registration,
+        local_bind_addr,
+        server_addr,
+        target_addr,
+    )
+    .await?;
+    let (observed_sender, mut observed_receiver) = mpsc::channel(1);
+    tokio::spawn(async move {
+        while let Some(event) = event_receiver.recv().await {
+            match event {
+                h3_masque::client::BoundProxyEvent::NotifyPublicAddress(public_addr) => {
+                    info!("Received public addresses: {}", public_addr);
+                }
+                h3_masque::client::BoundProxyEvent::NotifyObservedAddress {
+                    local_address,
+                    observed_address,
+                } => {
+                    info!(
+                        "Observed address for local address {} is {}",
+                        local_address, observed_address
+                    );
+                    observed_sender
+                        .send((local_address, observed_address))
+                        .await
+                        .unwrap();
+                }
+            }
+        }
+    });
+
+    let alpn = [msquic::BufferRef::from("h3")];
+
+    // create msquic-async listener
+    let configuration = msquic::Configuration::open(
+        &registration,
+        &alpn,
+        Some(
+            &msquic::Settings::new()
+                .set_IdleTimeoutMs(10000)
+                .set_KeepAliveIntervalMs(1000)
+                .set_PeerBidiStreamCount(100)
+                .set_PeerUnidiStreamCount(100)
+                .set_DatagramReceiveEnabled()
+                .set_StreamMultiReceiveEnabled()
+        ),
+    )?;
+
+    #[cfg(any(not(windows), feature = "quictls"))]
+    {
+        use std::io::Write;
+        use tempfile::NamedTempFile;
+
+        let cert = include_bytes!("../cert.pem");
+        let key = include_bytes!("../key.pem");
+
+        let mut cert_file = NamedTempFile::new()?;
+        cert_file.write_all(cert)?;
+        let cert_path = cert_file.into_temp_path();
+        let cert_path = cert_path.to_string_lossy().into_owned();
+
+        let mut key_file = NamedTempFile::new()?;
+        key_file.write_all(key)?;
+        let key_path = key_file.into_temp_path();
+        let key_path = key_path.to_string_lossy().into_owned();
+
+        let cred_config = msquic::CredentialConfig::new().set_credential(
+            msquic::Credential::CertificateFile(msquic::CertificateFile::new(key_path, cert_path)),
+        );
+
+        configuration.load_credential(&cred_config)?;
+    }
+
+    #[cfg(all(windows, not(feature = "quictls")))]
+    {
+        use schannel::RawPointer;
+        use schannel::cert_context::{CertContext, KeySpec};
+        use schannel::cert_store::{CertAdd, Memory};
+        use schannel::crypt_prov::{AcquireOptions, ProviderType};
+
+        let cert = include_str!("../cert.pem");
+        let key = include_bytes!("../key.pem");
+
+        let mut store = Memory::new().unwrap().into_store();
+
+        let name = String::from("msquic-async-example");
+
+        let cert_ctx = CertContext::from_pem(cert).unwrap();
+
+        let mut options = AcquireOptions::new();
+        options.container(&name);
+
+        let type_ = ProviderType::rsa_full();
+
+        let mut container = match options.acquire(type_) {
+            Ok(container) => container,
+            Err(_) => options.new_keyset(true).acquire(type_).unwrap(),
+        };
+        container.import().import_pkcs8_pem(key).unwrap();
+
+        cert_ctx
+            .set_key_prov_info()
+            .container(&name)
+            .type_(type_)
+            .keep_open(true)
+            .key_spec(KeySpec::key_exchange())
+            .set()
+            .unwrap();
+
+        let context = store.add_cert(&cert_ctx, CertAdd::Always).unwrap();
+
+        let cred_config = msquic::CredentialConfig::new().set_credential(
+            msquic::Credential::CertificateContext(unsafe { context.as_ptr() }),
+        );
+
+        configuration.load_credential(&cred_config)?;
+    };
+
+    let listener = msquic_async::Listener::new(&registration, configuration)?;
+
+    let addr: SocketAddr = "127.0.0.1:8443".parse()?;
+    listener.start(&alpn, Some(addr))?;
+    let server_addr = listener.local_addr()?;
+
+    info!("listening on {}", server_addr);
+
+    let Some((local_address, observed_address)) = observed_receiver.recv().await else {
+        error!("did not receive observed address");
+        return Ok(());
+    };
+
+    // handle incoming connections and requests
+
+    while let Ok(conn) = listener.accept().await {
+        info!("new connection established");
+        let local_address = local_address.clone();
+        let observed_address = observed_address.clone();
+        let event_handle = {
+            let conn = conn.clone();
+            conn.add_bound_addr(local_address.clone())?;
+            conn.add_observed_addr(local_address, observed_address)?;
+            info!(
+                "added bound address {} with observed address {}",
+                local_address, observed_address
+            );
+            tokio::spawn(async move {
+                while let Ok(event) = poll_fn(|cx| conn.poll_event(cx)).await {
+                    match event {
+                        msquic_async::ConnectionEvent::NotifyObservedAddress {
+                            local_address,
+                            observed_address,
+                        } => {
+                            info!(
+                                "local address: {}, observed address: {}",
+                                local_address, observed_address
+                            );
+                            // conn.add_local_addr(local_address.clone(), local_address)?;
+                        }
+                        msquic_async::ConnectionEvent::NotifyRemoteAddressAdded {
+                            address,
+                            sequence_number,
+                        } => {
+                            info!(
+                                "remote address: {}, sequence number: {}",
+                                address, sequence_number
+                            );
+                            // conn.create_path(unspecified_address.clone(), address.clone())?;
+                        }
+                        msquic_async::ConnectionEvent::PathValidated {
+                            local_address,
+                            remote_address,
+                        } => {
+                            info!(
+                                "path validated local address: {}, remote address: {}",
+                                local_address, remote_address
+                            );
+                        }
+                        msquic_async::ConnectionEvent::NotifyRemoteAddressRemoved {
+                            sequence_number,
+                        } => {
+                            info!(
+                                "remote address removed with sequence number: {}",
+                                sequence_number
+                            );
+                        }
+                    }
+                }
+                anyhow::Ok(())
+            })
+        };
+        let root = root.clone();
+        tokio::spawn(async move {
+            let mut h3_conn =
+                h3::server::Connection::new(h3_msquic_async::Connection::new(conn)).await?;
+            loop {
+                match h3_conn.accept().await {
+                    Ok(Some(req_resolver)) => {
+                        let (req, stream) = match req_resolver.resolve_request().await {
+                            Ok(req) => req,
+                            Err(err) => {
+                                error!("error resolving request: {}", err);
+                                continue;
+                            }
+                        };
+                        info!("new request: {:#?}", req);
+
+                        let root = root.clone();
+
+                        tokio::spawn(async move {
+                            if let Err(e) = handle_request(req, stream, root).await {
+                                error!("handling request failed: {}", e);
+                            }
+                        });
+                    }
+
+                    // indicating no more streams to be received
+                    Ok(None) => {
+                        break;
+                    }
+
+                    Err(err) => {
+                        error!("error on accept {}", err);
+                        break;
+                    }
+                }
+            }
+            event_handle.await??;
+            anyhow::Ok(())
+        });
+    }
+
+    Ok(())
+}
+
+async fn handle_request<T>(
+    req: Request<()>,
+    mut stream: RequestStream<T, Bytes>,
+    serve_root: Arc<Option<PathBuf>>,
+) -> anyhow::Result<()>
+where
+    T: BidiStream<Bytes>,
+{
+    let (status, to_serve) = match serve_root.as_deref() {
+        None => (StatusCode::OK, None),
+        Some(_) if req.uri().path().contains("..") => (StatusCode::NOT_FOUND, None),
+        Some(root) => {
+            let to_serve = root.join(req.uri().path().strip_prefix('/').unwrap_or(""));
+            match File::open(&to_serve).await {
+                Ok(file) => (StatusCode::OK, Some(file)),
+                Err(e) => {
+                    error!("failed to open: \"{}\": {}", to_serve.to_string_lossy(), e);
+                    (StatusCode::NOT_FOUND, None)
+                }
+            }
+        }
+    };
+
+    let resp = http::Response::builder().status(status).body(())?;
+
+    match stream.send_response(resp).await {
+        Ok(_) => {
+            info!("successfully respond to connection");
+        }
+        Err(err) => {
+            error!("unable to send response to connection peer: {:?}", err);
+        }
+    }
+
+    if let Some(mut file) = to_serve {
+        loop {
+            let mut buf = BytesMut::with_capacity(4096 * 10);
+            if file.read_buf(&mut buf).await? == 0 {
+                break;
+            }
+            stream.send_data(buf.freeze()).await?;
+        }
+    }
+
+    Ok(stream.finish().await?)
+}
